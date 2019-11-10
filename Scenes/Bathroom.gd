8944b70f5bc74808b2263dc2b3f08a14f614c29d
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-10 19:08:54 -0300

    Adding Tooth Brush Animation

--- Scenes/Bathroom.gd
+++ Scenes/Bathroom.gd
@@ -1,11 +1,20 @@
 extends Node
 
 var menino_escova = preload("res://Assets/Rotinas/Banheiro/menino_segurando_escova.png")
-onready var character = get_node("CanvasLayer/Character")
+var menino_pasta = preload("res://Assets/Rotinas/Banheiro/menino_colocando_pasta.png")
+var menino_escovando_dente = preload("res://Assets/Rotinas/Banheiro/menino_escovando_dente_parte1.png")
 
+onready var character = get_node("CanvasLayer/Character")
+onready var paste = get_node("CanvasLayer/Tooth Paste")
 
 func _on_Red_Brush_pressed():
 	get_node("CanvasLayer/Red Brush").set_disabled(true)
 	get_node("CanvasLayer/Red Brush").hide()
 	character.set_texture(menino_escova)
-	print("botao clicado")
+	get_node("CanvasLayer/Tooth Paste/TouchScreenButton").show()
+	get_node("CanvasLayer/Tooth Paste/Anim_Paste").play("Blink")
+	
+
+func _on_TouchScreenButton_pressed():
+	character.set_texture(menino_pasta)
+	paste.hide()
--- Scripts/Splash Screen.gd
+++ Scripts/Splash Screen.gd
@@ -1,6 +1,4 @@
 extends Node
 
-
-
 func _on_Timer_timeout():
 	Transition.fade_to("res://Scenes/Bathroom.tscn")
