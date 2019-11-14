5265b68b3a0b8e77417eb46263d429fe3aea6cb2
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-13 22:29:08 -0300

    Adding home screen

---
+++ Scenes/Main_Screen.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+
+func _ready():
+	pass
+
+
+func _on_Routines_btn_pressed():
+	print("routines pressed")
+	pass # replace with function body
--- Scripts/Splash_Screen.gd
+++ Scripts/Splash_Screen.gd
@@ -1,4 +1,4 @@
 extends Node
 
 func _on_Timer_timeout():
-	Transition.fade_to("res://Scenes/Bathroom.tscn")
+	Transition.fade_to("res://Scenes/Main_Screen.tscn")
