12fee42f1624f8e634082adc13611017ec3a7b76
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-14 00:24:21 -0300

    Creating register settings

--- Scenes/Main_Screen.gd
+++ Scenes/Main_Screen.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var password = ""
+onready var enter_button = get_node("Bar/Top_Menu/Enter_Button")
+onready var password_box = get_node("Bar/Top_Menu/Password_Box")
+onready var register_button = get_node("Bar/Top_Menu/Register")
+onready var password_box_register = get_node("Bar/Top_Menu/Password_Box_Register")
 
 func _ready():
 	pass
@@ -15,16 +19,24 @@ func _on_Menu_Button_pressed():
 
 
 func _on_Register_button_pressed():
+	enter_button.hide()
+	password_box.hide()
+	register_button.hide()
+	password_box_register.show()
 	print("register pressed")
 	pass # replace with function body
 
-
 func _on_Enter_Button_pressed():
-	password = get_node("Bar/Top_Menu/ColorFrame/LineEdit").get_text()
+	password = get_node("Bar/Top_Menu/Password_Box/current_password").get_text()
 	print(password)
-
 
 func _on_Cancel_btn_pressed():
 	get_node("Anim_Bar").play("Up_Bar")
-	print("cancel button pressed")
+
+func _on_ok_button_pressed():
+	enter_button.show()
+	password_box.show()
+	register_button.show()
+	password_box_register.hide()
+	print("ok pressed")
 	pass # replace with function body
