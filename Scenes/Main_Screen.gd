886246e0177b9a56ab52ae5c494664dd85edf50a
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-13 22:55:42 -0300

    Adding parents screen

--- Scenes/Main_Screen.gd
+++ Scenes/Main_Screen.gd
@@ -1,12 +1,13 @@
 extends Node
 
 
-
-
 func _ready():
 	pass
-
 
 func _on_Routines_btn_pressed():
 	print("routines pressed")
 	pass # replace with function body
+
+func _on_Menu_Button_pressed():
+	print("menu pressed")
+	get_node("Anim_Bar").play("Drop_Bar")
