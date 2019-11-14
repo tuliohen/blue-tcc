59bc843715f91a212b0fca6036888005075cdd8b
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-14 01:45:55 -0300

    Finishing save password

--- Scenes/Main_Screen.gd
+++ Scenes/Main_Screen.gd
@@ -1,14 +1,41 @@
 extends Node
 
 var password = ""
+
+var save_file = File.new()
+var save_path = "res://savegame.save"
+var save_data = {"password": ""}
+
 onready var enter_button = get_node("Bar/Top_Menu/Enter_Button")
 onready var password_box = get_node("Bar/Top_Menu/Password_Box")
 onready var register_button = get_node("Bar/Top_Menu/Register")
 onready var password_box_register = get_node("Bar/Top_Menu/Password_Box_Register")
 
 func _ready():
-	pass
+	print("password atual é: ", password)
+	if not save_file.file_exists(save_path):
+		create_save()
+	else:
+		load_password()
 
+func create_save():
+	save_file.open(save_path, File.WRITE)
+	save_file.store_var(save_data)
+	save_file.close()
+
+func save_password():
+	save_data["password"] = password
+	save_file.open(save_path, File.WRITE)
+	save_file.store_var(save_data)
+	save_file.close()
+
+func load_password():
+	save_file.open(save_path, File.READ)
+	save_data = save_file.get_var()
+	save_file.close()
+	password = save_data["password"]
+	print("loaded password: ", password)
+	
 func _on_Routines_btn_pressed():
 	print("routines pressed")
 	pass # replace with function body
@@ -38,5 +65,8 @@ func _on_ok_button_pressed():
 	password_box.show()
 	register_button.show()
 	password_box_register.hide()
+	password = get_node("Bar/Top_Menu/Password_Box_Register/new_password").get_text()
 	print("ok pressed")
+	save_password()
+	print(password)
 	pass # replace with function body
