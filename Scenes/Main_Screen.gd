12047ba120c8a709abb09b899a8e566ea514a17b
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-13 23:21:55 -0300

    Adding buttons scripts (mock)

--- Scenes/Main_Screen.gd
+++ Scenes/Main_Screen.gd
@@ -11,3 +11,13 @@ func _on_Routines_btn_pressed():
 func _on_Menu_Button_pressed():
 	print("menu pressed")
 	get_node("Anim_Bar").play("Drop_Bar")
+
+
+func _on_Register_button_pressed():
+	print("register pressed")
+	pass # replace with function body
+
+
+func _on_Enter_Button_pressed():
+	print("enter pressed")
+	pass # replace with function body
