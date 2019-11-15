7568f5873bbf3071e5b7997cf27c4506b8c9278d
Tulio <tulio.nobrega@ccc.ufcg.edu.br>
2019-11-15 16:14:22 -0300

    Implementing micro routine selection

--- Scenes/Parents_Screen.gd
+++ Scenes/Parents_Screen.gd
@@ -1,14 +1,66 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const IS_ROUTINE = false
+onready var label_crie_rotina = get_node("Micro_Routines/Micros_BG/Label_Crie_Rotina")
+
+var lista_de_clicks = []
+const COMER_PRESSED = false
+const DEVER_PRESSED = false
+const ESCOVAR_PRESSED = false
+const ESCOLA_PRESSED = false
+const BANHO_PRESSED = false
+const ROUPA_PRESSED = false
+const COMER = "COMER"
+const DEVER = "DEVER"
+const ESCOVAR = "ESCOVAR"
+const ESCOLA = "ESCOLA"
+const BANHO = "BANHO"
+const ROUPA = "ROUPA"
+
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	if IS_ROUTINE:
+		print("rotina criada")
+		label_crie_rotina.hide()
+	else:
+		label_crie_rotina.show()
 
+func _on_Create_routine_btn_pressed():
+	get_node("Micro_Routines/Micros_BG").show()
+	
+func _on_X_button_pressed():
+	get_node("Micro_Routines/Micros_BG").hide()
 
-func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/Main_Screen.tscn")
+func add_remove_list(flag, item):
+	if flag == true:
+		lista_de_clicks.append(item)
+	else:
+		for elem in lista_de_clicks:
+			if elem == item:
+				lista_de_clicks.erase(item)
+
+func _on_Comer_btn_pressed():
+	COMER_PRESSED = not COMER_PRESSED
+	add_remove_list(COMER_PRESSED, COMER)
+	print(lista_de_clicks)
+
+func _on_Dever_btn_pressed():
+	DEVER_PRESSED = not DEVER_PRESSED
+	add_remove_list(DEVER_PRESSED, DEVER)
+	print(lista_de_clicks)
+
+func _on_Escovar_btn_pressed():
+	ESCOVAR_PRESSED = not ESCOVAR_PRESSED
+	print("escovar")
+
+func _on_Escola_btn_pressed():
+	ESCOLA_PRESSED = not ESCOLA_PRESSED
+	print("escola")
+	
+func _on_Banho_btn_pressed():
+	BANHO_PRESSED = not BANHO_PRESSED
+	print("banho")
+	
+func _on_Roupa_btn_pressed():
+	ROUPA_PRESSED = not ROUPA_PRESSED
+	print("roupa")
